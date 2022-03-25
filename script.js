3cc1e8a386128f5a354197d83e3f8acc53d938a5
Lee Gusion <1951120099@sv.ut.edu.vn>
2022-03-26 00:43:38 +0700

    kiet cute

---
+++ script.js
@@ -0,0 +1,15 @@
+let slide1 = document.getElementById("slide1");
+let slide2 = document.getElementById("slide2");
+let slide3 = document.getElementById("slide3");
+
+setTimeout(() => {
+  slide1.style.transform = "translateY(-100%)";
+}, 1000);
+
+setTimeout(() => {
+  slide2.style.transform = "translateY(100%)";
+}, 2000);
+
+setTimeout(() => {
+  slide3.style.transform = "translateY(-100%)";
+}, 3000);
